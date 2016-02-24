89dfc8ecfd8943a141e548d91f183b91c9a317fa
Nyron Curtis Waite <Nyroncurtiswaite@gmail.com>
2016-02-24 09:45:50 -0500

    side changes

--- trivia.js
+++ trivia.js
@@ -3,134 +3,88 @@ var incorrect = 0;
 
 $(document).ready(function(){
 
- $("#correctguess").text(correct).css("color" , "blue");
- $("#incorrectguess").text(incorrect).css("color" , "red");
+ $("#correctguess").css("color" , "blue");
+ $("#incorrectguess").css("color" , "red");
 
 
 
 function firstquestion(){
 
-$("#question1").css("color" , "black").text("Is The Sky Blue?");
-$("#firstoption").text("Truth Button").on('click.first' , function(){
-var right = prompt("Type YES or NO").toLowerCase();
-if(right == "yes"){
-  alert("Correct!");
-  correct = correct + 2;
-  $("#correctguess").text(correct).css("color" , "blue");
-  secondquestion();
-}
-else if(right == "no"){
-  alert("Incorrect!");
-  incorrect = incorrect - 2;
-  $("#incorrectguess").text(incorrect).css("color" , "red");
-  secondquestion();
-}
-else{
-alert("Enter correct value");
-}
-});
+// $("#question1").css("color" , "black").text("Christopher Discovered America In 1492");
+// $("#firstoption").text("True").on('click.first' , function(){
+//   correct +=  2;
 
-}
+// // secondquestion();
+// //
+// // });
+// //
+// // $("#secondoption").text("False").on('click.first' , function(){
+// //   incorrect -= 2;
+// secondquestion();
+// });
+//
+// console.log('This');
 
+  var answer;
 
+  $("#firstoption").click(function(){
+    answer = 'true';
+    console.log('True');
+  })
 
+  $("#secondoption").click(function(){
+    answer = 'false';
+    console.log('False');
+  })
 
-function secondquestion(){
-  $("#question1").css("color" , "gray").text("Cats Sleep 16 to 18 hours a day?");
-  // remove the firstquesiton click event
-
-  $("#firstoption").off('click.first').on('click.second' ,function(){
-    var right = prompt("Type TRUE or FALSE").toLowerCase();
-    if(right == "true"){
-      alert("Correct");
-      correct++;
-    $("#correctguess").text(correct).css("color" , "blue");
-      thirdquestion();
-    }
-    else if(right == "false"){
-      alert("Incorrect!");
-      incorrect - 2;
-      $("#incorrectguess").text(incorrect).css("color" , "red");
-      thirdquestion();
-    }
-    else{
-      alert("Enter Correct value");
-    }
-  });
-}
-
-
-
-function thirdquestion(){
-
-  alert("Gold Round! If You Answer Correctly, You Will Gain +2 Or Lose -2")
-
-$("#question1").css("color" , "gold").text("1 in 5,000 north Atlantic lobsters are born bright blue");
-
-$("#firstoption").off('click.first').off('click.second').on('click.third' ,function(){
-  var right = prompt("Type TRUE or FALSE").toLowerCase();
-  if(right == "true" || right == "TRUE"){
-    alert("Correct");
-    correct = correct + 2
-    $("#correctguess").text(correct).css("color" , "gold");
-    fourthquestion();
+  if(answer == 'true'){
+    correct += 2;
+    $("#correctguess").text(correct);
   }
-  else if(right == "false"){
-    alert("Incorrect!");
-    incorrect = incorrect - 2;
-    $("#incorrectguess").text(incorrect).css("color" , "darkred");
-    fourthquestion();
+  if(answer == 'false'){
+    incorrect -= 2;
+    $("#correctguess").text(incorrect);
   }
-  else{
-    alert("Enter Correct value");
-  }
-});
-
+  //
+  // secondquestion();
 }
-
-function fourthquestion(){
-
-$("#question1").css("color" , "gray").text("Coke was invented in 1982");
-
-$("#firstoption").off('click.first').off('click.second').off('click.third').on('click.fourth' ,function(){
-  var right = prompt("Type TRUE or FALSE").toLowerCase();
-  if(right == "true" || right == "TRUE"){
-    alert("Correct");
-    correct++
-    $("#correctguess").text(correct).css("color" , "blue");
-  }
-  else if(right == "false" || "FALSE"){
-    alert("Incorrect!");
-    incorrect++
-    $("#incorrectguess").text(incorrect).css("color" , "red");
-  }
-  else{
-    alert("Enter Correct value");
-  }
-});
-
-}
-
-
-
-
-
-function restart(){
-
-$("#restart").on("click" , function(){
-
-correct = 0;
-incorrect = 0;
-$("#correctguess").text(correct).css("color" , "blue");
-$("#incorrectguess").text(incorrect).css("color" , "red");
-
-
 })
 
-}
+// function secondquestion(){
+//
+// $("#question1").css("color" , "black").text("Cats Sleep 16 to 18 hours a day?");
+// $("#firstoption").off('click.first').on('click.second' , function(){
+//   correct += 1;
+//   $("#correctguess").text(correct).css("color" , "gold");
+//
+//
+// });
+// $("#secondoption").off('click.first').on('click.second' , function(){
+//   incorrect -= 1;
+//   $("#incorrectguess").text(incorrect).css("color" , "gold");
+//
+//
+// })
+// }
+// }
+
+// function restart() {
+// $("#restart").click(function(){
+//
+// correct = 0;
+// incorrect = 0;
+//
+// $("#correctguess").text(correct).css("color" , "blue");
+// $("#incorrectguess").text(incorrect).css("color" , "red");
+//
+// firstquestion();
+//
+// })
+//
+// }
+
+// firstquestion();
 
 
-firstquestion();
 
-
-});
+//});
