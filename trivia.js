16db06eace069f7c7f7195072c480935c65b53f8
Nyron Curtis Waite <Nyroncurtiswaite@gmail.com>
2016-02-25 01:08:47 -0500

    testing

--- trivia.js
+++ trivia.js
@@ -2,9 +2,10 @@
 
 $(document).ready(function(){
 
- $("#firstoption").text("Click Here");
+ $("#firstoption").text("True");
+ $("#secondoption").text("False");
  $("#correctguess").css("color" , "blue");
- $("#incorrectguess").css("color" , "darkred");
+ $("#incorrectguess").css("color" , "red");
  $("#question1").css("color" , "blue");
 
  var correct = 0;
@@ -17,116 +18,78 @@ function firstquestion(){
 
 $("#question1").text("Is The Sky Blue?");
 $("#firstoption").on('click.first' , function(){
-var right = prompt("Type yes or no").toLowerCase();
-if(right == "yes"){
   alert("Correct!");
   correct++;
   $("#correctguess").text(correct);
   secondquestion();
-}
-else if(right == "no"){
+})
+$("#secondoption").on('click.first' , function(){
   alert("Incorrect!");
   incorrect++;
   $("#incorrectguess").text(incorrect);
   secondquestion();
+})
 }
-});
-}
-
-
-
 
 function secondquestion(){
 
-  $("#question1").text("Cats Sleep 16 to 18 hours a day?");
-  $("#firstoption").off('click.first').on('click.second' ,function(){
-    var right = prompt("Type yes or no").toLowerCase();
-    if(right == "yes"){
-      alert("Correct");
-      correct++;
-    $("#correctguess").text(correct);
-      thirdquestion();
-    }
-    else if(right == "no"){
-      alert("Incorrect!");
-      incorrect++;
-      $("#incorrectguess").text(incorrect)
-      thirdquestion();
-    }
-  });
+$("#question1").text("Cats Sleep 16 to 18 hours a day?");
+$("#firstoption").off('click.first').on('click.second' , function(){
+  alert("Correct!");
+  correct++;
+  $("#correctguess").text(correct);
+  thirdquestion();
+})
+$("#secondoption").off('click.first').on('click.second' , function(){
+  alert("Incorrect!");
+  incorrect++;
+  $("#incorrectguess").text(incorrect);
+  thirdquestion();
+})
 }
-
-
 
 function thirdquestion(){
 
 $("#question1").text("1 in 5,000 north Atlantic lobsters are born bright blue");
+$("#firstoption").off('click.first').off('click.second').on('click.third' , function(){
+  alert("Correct!");
+  correct++;
+  $("#correctguess").text(correct);
 
-$("#firstoption").off('click.first').off('click.second').on('click.third' ,function(){
-  var right = prompt("Type yes or no").toLowerCase();
-  if(right == "yes"){
-    alert("Correct");
-    correct = correct + 2;
-    $("#correctguess").text(correct);
-    fourthquestion();
-  }
-  if(correct > 2){
-    alert("You Win! Game Over");
-    restart();
-  }
-  else if(right == "no"){
-    alert("Incorrect!");
-    incorrect = incorrect + 2;
-    $("#incorrectguess").text(incorrect);
-    fourthquestion();
-  }
-  if(incorrect > 2){
-    alert("You Lose! Game Over");
-    restart();
-  }
-});
+  fouthquestion();
+})
+$("#secondoption").off('click.first').off('click.second').on('click.third' , function(){
+  alert("Incorrect!");
+  incorrect++;
+  $("#incorrectguess").text(incorrect);
 
+  fouthquestion();
+})
+if(correct >= 2){
+  restart();
+}
+if(incorrect >= 2){
+  restart();
+}
 }
 
-function fourthquestion(){
+function fouthquestion(){
 
-$("#question1").text("Coke was invented in 1982");
-
-$("#firstoption").off('click.first').off('click.second').off('click.third').on('click.fourth' ,function(){
-  var right = prompt("Type yes or no").toLowerCase();
-  if(right == "yes"){
-    alert("Correct");
-    correct++
-    $("#correctguess").text(correct);
-  }
-  if(correct > 2){
-    alert("You Win! Game Over");
-    restart();
-  }
-
-  else if(right == "no"){
-    alert("Incorrect!");
-    incorrect++
-    $("#incorrectguess").text(incorrect);
-    }
-    if(incorrect > 2){
-      alert("You Lose! Game Over");
-      restart();
-  }
-});
-
+$("#question1").text("Hi?");
+$("#firstoption").on('click.first' , function(){
+  alert("Correct!");
+  correct++;
+  $("#correctguess").text(correct);
+  secondquestion();
+})
+$("#secondoption").on('click.first' , function(){
+  alert("Incorrect!");
+  incorrect++;
+  $("#incorrectguess").text(incorrect);
+  secondquestion();
+})
 }
 
-// function endgame(){
-// if(correct > 1){
-//   alert("Game Over");
-// }
-// else{
-//   firstquestion();
-// }
-// }
-//
-// endgame()
 
 
 function restart(){
@@ -135,11 +98,13 @@ correct = 0;
 incorrect = 0;
 $("#correctguess").text(correct).css("color" , "blue");
 $("#incorrectguess").text(incorrect).css("color" , "red");
-firstquestion();
+$("#question1").text("Is The Sky Blue?");
 
 
 }
+
 firstquestion();
+
 })
 
 //
